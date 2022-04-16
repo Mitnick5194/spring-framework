42bb872f43fa1a3198d5d74c6800ea4ab93e10d6
niezhenjie <niezhenjie@bluemoon.com.cn>
2022-04-16 16:19:26 +0800

    事务

--- mine-demo/src/main/java/com/ajie/demo/config/BeanConfig.java
+++ mine-demo/src/main/java/com/ajie/demo/config/BeanConfig.java
@@ -1,8 +1,17 @@
 package com.ajie.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * @Author: niezhenjie
@@ -10,8 +19,28 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @EnableAspectJAutoProxy
-@ComponentScan(basePackages={"com.ajie.demo"})
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"com.ajie.demo"})
+@Configuration
 //@Import({LogAopAspect.class})
 public class BeanConfig {
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+	@Bean
+	public PlatformTransactionManager platformTransactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl(System.getProperty("jdbc.url"));
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword(System.getProperty("jdbc.mysql.passwd"));
+		return dataSource;
+	}
 
 }
